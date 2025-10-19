aa52420868613482f9ba341a2da209345c689f2f
noah <qkrwldn0818@naver.com>
2025-10-19 17:07:19 +0900

    fix : base url 수정 cs-study -> CS-Study

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { resolve } from 'path';
 
 export default defineConfig({
   base: process.env.NODE_ENV === 'production'
-    ? '/cs-study/'
+    ? '/CS-Study/'
     : '/',
   build: {
     outDir: 'dist',
